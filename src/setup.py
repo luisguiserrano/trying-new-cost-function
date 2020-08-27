b3cd6c67c99b9339793ef873e801f4ea318f2a9f
luisguiserrano <luisgui.serrano@gmail.com>
2020-08-27 09:27:25 -0400

    Small fix

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ import setuptools
 import os
 
 setuptools.setup(
-    name="luis-trying-cost-function",
+    name="trying-new-cost-function",
     version="0.2.0",
     url="https://github.com/luisguiserrano/trying-new-cost-function",
     packages=setuptools.find_namespace_packages(include=['zquantum.*', 'kl.*']),
