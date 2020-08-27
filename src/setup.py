fe9941a39b4a66c82795a5515da8128b0180b37c
luisguiserrano <luisgui.serrano@gmail.com>
2020-08-27 10:07:20 -0400

    Small fix

--- src/setup.py
+++ src/setup.py
@@ -1,18 +1,12 @@
 import setuptools
-import os
 
 setuptools.setup(
-    name="trying-new-cost-function",
-    version="0.2.0",
-    url="https://github.com/luisguiserrano/trying-new-cost-function",
-    packages=setuptools.find_namespace_packages(include=['zquantum.*']),
-    packages = setuptools.find_packages(where = "python")
-    package_dir={'' : 'python'},
+    name="cost-functions",
+    packages=setuptools.find_packages(where="python"),
+    package_dir={"": "python"},
     classifiers=(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ),
-    install_requires=[
-        'z-quantum-core',# 'trying-new-cost-function'
-    ]
+    install_requires=["z-quantum-core",],
 )
