1aa41692ab8c36b27d12e5cea16c0e036b73a74c
luisguiserrano <luisgui.serrano@gmail.com>
2020-08-27 09:30:08 -0400

    Fixing setup

--- src/setup.py
+++ src/setup.py
@@ -5,13 +5,14 @@ setuptools.setup(
     name="trying-new-cost-function",
     version="0.2.0",
     url="https://github.com/luisguiserrano/trying-new-cost-function",
-    packages=setuptools.find_namespace_packages(include=['zquantum.*', 'kl.*']),
+    #packages=setuptools.find_namespace_packages(include=['zquantum.*', 'kl.*']),
+    packages = setuptools.find_packages(where = "python")
     package_dir={'' : 'python'},
     classifiers=(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ),
     install_requires=[
-        'z-quantum-core', 'trying-new-cost-function'
+        'z-quantum-core',# 'trying-new-cost-function'
     ]
 )
